8265998dd8cee83154fd490477a16221d9ff1e3d
andreizakroev-design <andrei.zakroev@gmail.com>
2026-07-26 23:17:09 +0300

    Fix display.h - replace escaped newlines with actual line breaks

--- src/display.h
+++ src/display.h
@@ -1,1 +1,17 @@
-#ifndef DISPLAY_H\n#define DISPLAY_H\n\n#include \"config.h\"\n#include \"sensors.h\"\n\nvoid display_init();\nvoid display_update();\nvoid display_show_main_screen();\nvoid display_next_screen();\nvoid display_enter_menu();\nvoid display_exit_menu();\nbool display_is_in_menu();\nuint8_t display_get_menu_item();\nuint8_t display_get_current_screen();\n\n#endif // DISPLAY_H\n
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "config.h"
+#include "sensors.h"
+
+void display_init();
+void display_update();
+void display_show_main_screen();
+void display_next_screen();
+void display_enter_menu();
+void display_exit_menu();
+bool display_is_in_menu();
+uint8_t display_get_menu_item();
+uint8_t display_get_current_screen();
+
+#endif // DISPLAY_H
